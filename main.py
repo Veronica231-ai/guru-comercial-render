0c70616e0b68b158097c406e73775aa64543ce1d
Veronica231-ai <romandini@outlook.com>
2026-06-10 15:30:33 -0300

    config render

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
+import os
+
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request
 
 app = App(
-    token="TOKEN_AQUI",
-    signing_secret="SIGNING_SECRET_AQUI"
+    token=os.environ["SLACK_BOT_TOKEN"],
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"]
 )
+
+flask_app = Flask(__name__)
 
 CANAL_COMUNICADOS = "#comunicações-"
 
 @app.command("/comunicado")
-def comunicado(ack, respond, command):
+def comunicado(ack, command):
+
     ack()
 
     texto = command["text"]
 
-    if not texto:
-        respond("Escreva uma mensagem após o comando.")
-        return
-
     app.client.chat_postMessage(
         channel=CANAL_COMUNICADOS,
-        text=f"📢 *Comunicado Guru Comercial*\n\n{texto}"
+        text=f"📢 {texto}"
     )
 
-    respond("✅ Comunicado enviado pelo Guru Comercial.")
-
-flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
 
-@flask_app.route("/", methods=["POST"])
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
